21fb6ab6a053f7e106929f3ffb71207fa2ae2a3b
QuocHung <tqhung1234@gmail.com>
2022-05-11 16:48:04 +0700

    update view

---
+++ app/src/main/java/com/example/bookstore/Book.java
@@ -0,0 +1,73 @@
+package com.example.bookstore;
+
+public class Book {
+    private String name,author,description,imgUrl;
+    private int price;
+    private int drawableResources;
+
+    public Book(int drawableResources,String name) {
+        this.drawableResources = drawableResources;
+        this.name = name;
+    }
+
+    public Book(int drawableResources) {
+        this.drawableResources = drawableResources;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getImgUrl() {
+        return imgUrl;
+    }
+
+    public void setImgUrl(String imgUrl) {
+        this.imgUrl = imgUrl;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    public void setPrice(int price) {
+        this.price = price;
+    }
+
+    public int getDrawableResources() {
+        return drawableResources;
+    }
+
+    public void setDrawableResources(int drawableResources) {
+        this.drawableResources = drawableResources;
+    }
+
+    public Book(String name, String author, String description, String imgUrl, int price, int drawableResources) {
+        this.name = name;
+        this.author = author;
+        this.description = description;
+        this.imgUrl = imgUrl;
+        this.price = price;
+        this.drawableResources = drawableResources;
+    }
+}
---
+++ app/src/main/java/com/example/bookstore/BookAdapter.java
@@ -0,0 +1,56 @@
+package com.example.bookstore;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.constraintlayout.helper.widget.Carousel;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.List;
+import java.util.Objects;
+
+public class BookAdapter extends RecyclerView.Adapter<BookAdapter.bookviewholder> {
+    List<Book> data;
+
+    public BookAdapter(List<Book> data){
+        this.data = data;
+    }
+
+    @NonNull
+    @Override
+    public bookviewholder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.custom_grid,parent,false);
+        return new bookviewholder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull bookviewholder holder, int position) {
+        holder.name.setText(data.get(position).getName());
+        holder.imgBook.setImageResource(data.get(position).getDrawableResources());
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return data.size();
+    }
+
+    public class bookviewholder extends RecyclerView.ViewHolder{
+
+        ImageView imgBook, imgFav;
+        TextView name;
+
+        public bookviewholder(@NonNull View itemView) {
+            super(itemView);
+
+            imgBook = itemView.findViewById(R.id.book_image);
+            name = itemView.findViewById(R.id.book_name);
+//            imgFav = itemView.findViewById(R.id.fav_icon);
+        }
+    }
+
+}
