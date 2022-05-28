d602073c97a7bcd5e6620ec802f5e052f30d3c6c
QuocHung <tqhung1234@gmail.com>
2022-05-28 22:42:18 +0700

    update logoutbutton

--- app/src/main/java/com/example/bookstore/ProfileFragment.java
+++ app/src/main/java/com/example/bookstore/ProfileFragment.java
@@ -4,15 +4,22 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageButton;
 
 import androidx.fragment.app.Fragment;
+
+import com.example.bookstore.databinding.FragmentProfileBinding;
+import com.example.bookstore.model.User;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DatabaseReference;
+
 
 /**
  * A simple {@link Fragment} subclass.
  * Use the {@link ProfileFragment#newInstance} factory method to
  * create an instance of this fragment.
  */
-public class ProfileFragment extends Fragment {
+public class ProfileFragment extends Fragment implements View.OnClickListener {
 
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -22,6 +29,12 @@ public class ProfileFragment extends Fragment {
     // TODO: Rename and change types of parameters
     private String mParam1;
     private String mParam2;
+    private FragmentProfileBinding binding;
+    private FirebaseAuth firebaseAuth;
+    private View view;
+    private User email;
+    private User name;
+    private DatabaseReference databaseReference;
 
     public ProfileFragment() {
         // Required empty public constructor
@@ -52,12 +65,50 @@ public class ProfileFragment extends Fragment {
             mParam1 = getArguments().getString(ARG_PARAM1);
             mParam2 = getArguments().getString(ARG_PARAM2);
         }
+
+//        databaseReference = FirebaseDatabase.getInstance().getReference("Users");
+//        databaseReference.addListenerForSingleValueEvent(new ValueEventListener() {
+//            @Override
+//            public void onDataChange(@NonNull DataSnapshot snapshot) {
+//                User user = snapshot.getValue(User.class);
+//
+//                name = new User(getContext(),name)
+//            }
+//
+//            @Override
+//            public void onCancelled(@NonNull DatabaseError error) {
+//
+//            }
+//        });
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_profile, container, false);
+        firebaseAuth = FirebaseAuth.getInstance();
+        view = inflater.inflate(R.layout.fragment_profile, container, false);
+
+        configureImageButton();
+        return view;
+    }
+    private void configureImageButton() {
+        // TODO Auto-generated method stub
+        ImageButton logoutBt=(ImageButton) view.findViewById(R.id.btlogout);
+
+        logoutBt.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                firebaseAuth.signOut();
+            }
+        });
+
+
+    }
+
+
+    @Override
+    public void onClick(View view) {
+
     }
 }
